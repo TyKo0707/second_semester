2c72b060ea74103186e958a0c8c1c32a2351a22d
TyKo0707 <super.family.timut@gmail.com>
2022-02-11 13:11:12 +0200

    ADS first lab code update 1

--- ADS/lab1/lab1.py
+++ ADS/lab1/lab1.py
@@ -1,11 +1,8 @@
-# Массив: лучший, хороший, нормальный
-# Таймер
-#
-
 import time
 import matplotlib.pyplot as plt
 import random
 from sort_algorithms import *
+import math
 
 
 class TimerError(Exception):
@@ -38,37 +35,102 @@ def check_b(matrix, algo: str):
     for i in range(3):
         t.start()
         if algo == 'b':
-            bubble_sort(matrix[i])
+            bubble_comp, bubble_swap = bubble_sort(matrix[i])
+            res.append(bubble_comp)
+            res.append(bubble_swap)
         elif algo == 'c':
-            comb_sort(matrix[i])
+            comb_comp, comb_swap = comb_sort(matrix[i])
+            res.append(comb_comp)
+            res.append(comb_swap)
         time_b = t.result()
         res.append(time_b)
-    return res
+    return tuple(res)
 
 
 if __name__ == "__main__":
     arr_num = [10, 100, 1000, 5000, 10000, 20000, 50000]
-    res_bubble = []
-    res_comb = []
+    square = [i ** 2 for i in arr_num]
+    log_f = [i * math.log2(i) for i in arr_num]
+    res_bubble = [[], [], []]
+    res_comb = [[], [], []]
+    res_bubble_p = [[], [], []]
+    res_comb_p = [[], [], []]
     t = Timer()
-    for i in range(len(arr_num)):
-        best = [x for x in range(1, arr_num[i] + 1)]
-        worst = [y for y in range(arr_num[i], 0, -1)]
-        norm = random.sample(range(1, arr_num[i] + 1), arr_num[i])
-        if i == 0 or i == 1:
-            print(f'\nBest array: {best}, {len(best)}\n'
-                  f'Worst array: {worst}, {len(worst)}\n'
-                  f'Norm array: {norm}, {len(norm)}\n')
-        matrix = [best, worst, norm]
-        bubble_best, bubble_worst, bubble_norm = check_b(matrix, 'b')
-        print(f'Time for bubble sort(best): {bubble_best}\n'
-              f'Time for bubble sort(worst): {bubble_worst}\n'
-              f'Time for bubble sort(normal): {bubble_norm}')
-        if i == 0 or i == 1:
-            print(f'Best array after sort: {matrix[0]}, {len(matrix[0])}\n'
-                  f'Worst array after sort: {matrix[1]}, {len(matrix[1])}\n'
-                  f'Norm array after sort: {matrix[2]}, {len(matrix[2])}\n')
-        comb_best, comb_worst, comb_norm = check_b(matrix, 'c')
-        print(f'Time for comb sort(best): {comb_best}\n'
-              f'Time for comb sort(worst): {comb_worst}\n'
-              f'Time for comb sort(normal): {comb_norm}')
+    with open("result.txt", "w") as file:
+        for i in range(len(arr_num)):
+            best = [x for x in range(1, arr_num[i] + 1)]
+            worst = [y for y in range(arr_num[i], 0, -1)]
+            norm = random.sample(range(1, arr_num[i] + 1), arr_num[i])
+            matrix = [best[:], worst[:], norm[:]]
+            bubble_best_comp, bubble_best_swap, bubble_best, bubble_worst_comp, bubble_worst_swap, bubble_worst, \
+            bubble_norm_comp, bubble_norm_swap, bubble_norm = check_b(matrix, 'b')
+            res_bubble[0].append(bubble_best)
+            res_bubble[1].append(bubble_worst)
+            res_bubble[2].append(bubble_norm)
+            res_bubble_p[0].append(bubble_best_comp)
+            res_bubble_p[1].append(bubble_worst_comp)
+            res_bubble_p[2].append(bubble_norm_comp)
+            title = f'Array with {arr_num[i]} numbers:\n' \
+                    f'\tBest array: {best[:10]}...\n' \
+                    f'\tWorst array: {worst[:10]}...\n' \
+                    f'\tNorm array: {norm[:10]}...\n'
+            stw_bubble = f'\n\tBubble:\n' \
+                         f'\t\tNumber of comparisons, number of swaps in bubble sort(best): {bubble_best_comp}, {bubble_best_swap}\n' \
+                         f'\t\tTime for bubble sort(best): {bubble_best} s\n' \
+                         f'\n\t\tNumber of comparisons, number of swaps in bubble sort(worst): {bubble_worst_comp}, {bubble_worst_swap}\n' \
+                         f'\t\tTime for bubble sort(worst): {bubble_worst} s\n' \
+                         f'\n\t\tNumber of comparisons, number of swaps in bubble sort(norm): {bubble_norm_comp}, {bubble_norm_swap}\n' \
+                         f'\t\tTime for bubble sort(normal): {bubble_norm} s'
+            matrix = [best[:], worst[:], norm[:]]
+            comb_best_comp, comb_best_swap, comb_best, comb_worst_comp, comb_worst_swap, comb_worst, \
+            comb_norm_comp, comb_norm_swap, comb_norm = check_b(matrix, 'c')
+            res_comb[0].append(comb_best)
+            res_comb[1].append(comb_worst)
+            res_comb[2].append(comb_norm)
+            res_comb_p[0].append(comb_best_comp)
+            res_comb_p[1].append(comb_worst_comp)
+            res_comb_p[2].append(comb_norm_comp)
+            stw_comb = f'\n\n\tComb:\n' \
+                       f'\t\tNumber of comparisons, number of swaps in comb sort(best): {comb_best_comp}, {comb_best_swap}\n' \
+                       f'\t\tTime for bubble sort(best): {comb_best} s\n' \
+                       f'\n\t\tNumber of comparisons, number of swaps in comb sort(worst): {comb_worst_comp}, {comb_worst_swap}\n' \
+                       f'\t\tTime for bubble sort(worst): {comb_worst} s\n' \
+                       f'\n\t\tNumber of comparisons, number of swaps in comb sort(norm): {comb_norm_comp}, {comb_norm_swap}\n' \
+                       f'\t\tTime for bubble sort(normal): {comb_norm} s\n\n'
+
+            file.write(title)
+            file.write(stw_bubble)
+            file.write(stw_comb)
+
+    plt.figure()
+    ax1 = plt.subplot(2, 2, 1)
+    plt.plot(arr_num, res_bubble[0], '-', arr_num, res_bubble[1], '-', arr_num, res_bubble[2], '-')
+    plt.title('Bubble sort time')
+    plt.xlabel('Number of elements in array')
+    plt.ylabel('Time to sort')
+    plt.legend(['Best case', 'Worst case', 'Random case'])
+    plt.xticks(arr_num)
+    ax2 = plt.subplot(2, 2, 2)
+    plt.plot(arr_num, res_comb[0], '-', arr_num, res_comb[1], '-', arr_num, res_comb[2], '-')
+    plt.title('Comb sort time')
+    plt.xlabel('Number of elements in array')
+    plt.ylabel('Time to sort')
+    plt.legend(['Best case', 'Worst case', 'Random case'])
+    plt.xticks(arr_num)
+    ax3 = plt.subplot(2, 2, 3)
+    plt.plot(arr_num, res_bubble_p[0], '-', arr_num, res_bubble_p[1], '-', arr_num, res_bubble_p[2], '-')
+    plt.plot(arr_num, square, '-', alpha=0.5)
+    plt.title('Bubble sort performance')
+    plt.xlabel('Number of elements in array')
+    plt.ylabel('Number of comparisons')
+    plt.legend(['Best case', 'Worst case', 'Random case'])
+    plt.xticks(arr_num)
+    ax4 = plt.subplot(2, 2, 4)
+    plt.plot(arr_num, res_comb_p[0], '-', arr_num, res_comb_p[1], '-', arr_num, res_comb_p[2], '-')
+    plt.plot(arr_num, log_f, '-', alpha=0.5)
+    plt.title('Comb sort performance')
+    plt.xlabel('Number of elements in array')
+    plt.ylabel('Number of comparisons')
+    plt.legend(['Best case', 'Worst case', 'Random case'])
+    plt.xticks(arr_num)
+    plt.show()
--- ADS/lab1/sort_algorithms.py
+++ ADS/lab1/sort_algorithms.py
@@ -6,7 +6,7 @@ def bubble_sort(arr):
     i = 0
     while swapped:
         swapped = False
-        for j in range(0, n - i - 2):
+        for j in range(0, n - i - 1):
             bubble_comp += 1
             if arr[j] > arr[j + 1]:
                 bubble_swap += 1
@@ -20,10 +20,16 @@ def comb_sort(arr):
     len_a = len(arr)
     gap_k = 1.2473309
     gap = int(len_a / gap_k) if len_a > 1 else 0
+    comb_comp = 0
+    comb_swap = 0
     while gap:
         swapped = False
         for i in range(len_a - gap):
+            comb_comp += 1
             if arr[i + gap] < arr[i]:
+                comb_swap += 1
                 arr[i], arr[i + gap] = arr[i + gap], arr[i]
                 swapped = True
         gap = int(gap / gap_k) or swapped
+
+    return comb_comp, comb_swap
