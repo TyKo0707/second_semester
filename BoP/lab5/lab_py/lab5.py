daf57c0e23089b91a1e471df8051985e36d213d9
Timur Khitrick <super.family.timut@gmail.com>
2022-05-21 20:01:57 +0300

    Fifth lab python code

--- BoP/lab5/lab_py/lab5.py
+++ BoP/lab5/lab_py/lab5.py
@@ -1,8 +1,41 @@
 from material_point import MaterialPointX, MaterialPointXY
 
-a = MaterialPointX()
-a.get_attr()
+k = int(input("Input k: "))
+n = int(input("Input n: "))
+t = float(input("Input t: "))
 
-b = MaterialPointXY()
-b.get_attr()
+coords = []
+distances = []
+xs = []
+xys = []
 
+for i in range(k):
+    cl = MaterialPointX(t)
+    xs.append(cl)
+    print(cl)
+    for j in range(i):
+        dist = cl.get_distance(xs[j].get_x(), xs[j].get_y())
+        coordinates = [cl.get_x(), cl.get_y(), xs[j].get_x(), xs[j].get_y()]
+        coords.append(coordinates)
+        distances.append(dist)
+
+for i in range(n):
+    cl = MaterialPointXY(t)
+    xys.append(cl)
+    print(cl)
+    for j in range(i):
+        dist = cl.get_distance(xys[j].get_x(), xys[j].get_y())
+        coordinates = [cl.get_x(), cl.get_y(), xys[j].get_x(), xys[j].get_y()]
+        coords.append(coordinates)
+        distances.append(dist)
+
+for i in range(k):
+    for j in range(n):
+        dist = xs[i].get_distance(xys[j].get_x(), xys[j].get_y())
+        coordinates = [xs[i].get_x(), xs[i].get_y(), xys[j].get_x(), xys[j].get_y()]
+        coords.append(coordinates)
+        distances.append(dist)
+
+ind = distances.index(max(distances))
+print(f'Max distance: {distances[ind]}')
+print(f'Between points with coordinates: ({coords[ind][0]}; {coords[ind][1]}), ({coords[ind][2]}; {coords[ind][3]})')
--- BoP/lab5/lab_py/material_point.py
+++ BoP/lab5/lab_py/material_point.py
@@ -1,17 +1,69 @@
 from abc import ABCMeta, abstractmethod, ABC
+import random
+import math
 
 
 class MaterialPoint(metaclass=ABCMeta):
+    def __init__(self, t):
+        self._a1, self._a2, self._x0, self._y0 = random.randint(0, 5), random.randint(0, 5), \
+                                                 random.randint(0, 5), random.randint(0, 5)
+        self._t = t
+        self._x, self._y = self._find_x(), self._find_y()
+
+    def __str__(self):
+        return f'(x;y) = ({self._x}; {self._y})'
+
     @abstractmethod
-    def get_attr(self):
-        pass
+    def _find_x(self): pass
+
+    @abstractmethod
+    def _find_y(self): pass
+
+    @abstractmethod
+    def get_x(self): pass
+
+    @abstractmethod
+    def get_y(self): pass
+
+    @abstractmethod
+    def get_distance(self, ox, oy): pass
 
 
 class MaterialPointX(MaterialPoint, ABC):
-    def get_attr(self):
-        print('a')
+    def __init__(self, t):
+        super().__init__(t)
+
+    def _find_x(self):
+        return round(self._x0 + self._a1 * math.sin(self._t), 4)
+
+    def get_x(self):
+        return self._x
+
+    def _find_y(self):
+        return 0
+
+    def get_y(self):
+        return self._y
+
+    def get_distance(self, ox, oy):
+        return round(math.sqrt((self._x - ox) ** 2 + (self._y - oy) ** 2), 4)
 
 
 class MaterialPointXY(MaterialPoint, ABC):
-    def get_attr(self):
-        print('b')
+    def __init__(self, t):
+        super().__init__(t)
+
+    def _find_x(self):
+        return round(self._x0 + self._a1 * math.sin(self._t), 4)
+
+    def get_x(self):
+        return self._x
+
+    def _find_y(self):
+        return round(self._y0 + self._a2 * math.cos(self._t), 4)
+
+    def get_y(self):
+        return self._y
+
+    def get_distance(self, ox, oy):
+        return round(math.sqrt((self._x - ox) ** 2 + (self._y - oy) ** 2), 4)
